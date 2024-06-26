415080228c4cd5692eb6ec7f0a84d6b11d8f831d
nolans12 <stevenson.r.nolan@gmail.com>
2024-06-26 11:23:09 -0600

    Update earthSimulation.m

--- earthSimulation.m
+++ earthSimulation.m
@@ -14,7 +14,12 @@ set(earth, 'CData', topo);
 % Set axis properties
 axis equal;
 axis vis3d;
-set(gca, 'Color', 'k');
+set(gca, 'Color', [0 0.5 1]); % Set background color to blue
+
+% Set viewing limits bigger than the Earth
+xlim([-1.5 1.5]);
+ylim([-1.5 1.5]);
+zlim([-1.5 1.5]);
 
 % Add lighting
 light('Position', [1 0 1], 'Style', 'infinite');
